007ddbd0b3d278e389f8712445c50851a781daa7
Nick Felker <felker.nick@bk.ru>
2024-09-24 11:59:56 +0300

    upload base

---
+++ src/task_1/task_1.cpp
@@ -0,0 +1,7 @@
+#include "task_1.hpp"
+
+
+bool task_1()
+{
+    
+}
---
+++ src/task_1/task_1.hpp
@@ -0,0 +1,14 @@
+#ifndef WINDOW_EVENTS_H_
+#define WINDOW_EVENTS_H_
+
+#include <iostream>
+#include <stdlib.h>
+
+
+bool task_1();
+
+
+
+
+
+#endif
